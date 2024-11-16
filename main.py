8748a7c2720e2c82dfbef465b538e41d30045f54
Fabio Kusaba <fabinhokb44@gmail.com>
2024-11-16 19:11:57 -0300

    Criação das classes de request e response, atualização do retorno dos métodos get_all e get_by_id, e criação do método create POST /fila

--- main.py
+++ main.py
@@ -17,6 +17,24 @@ class Cliente(BaseModel):
     status_atendimento: bool
 
 
+# Request
+class ClienteRequest(BaseModel):
+    nome: constr(max_length=20)
+    tipo_atendimento: Literal['N', 'P']
+
+
+# Response
+class ClienteResponse:
+    posicao: int
+    nome: str
+    data_chegada: datetime
+
+    def __init__(self, posicao: int, nome: str, data_chegada: datetime):
+        self.posicao = posicao
+        self.nome = nome
+        self.data_chegada = data_chegada
+
+
 # Seed de dados
 db_clientes: List[Cliente] = [
     Cliente(id=1, nome="Maria", tipo_atendimento='P', data=datetime.now(), status_atendimento=False),
@@ -27,18 +45,34 @@ db_clientes: List[Cliente] = [
 
 @app.get("/fila")
 def get_all():
-    resposta = []
+    resposta: List[ClienteResponse] = []
     if len(db_clientes) == 0:
-        return {"data": db_clientes, "status": 200}
+        return {"info": db_clientes, "status": 200}
     for cliente in db_clientes:
         if cliente.status_atendimento == False:
-            resposta.append({"posicao": cliente.id, "nome": cliente.nome, "data_chegada": cliente.data})
-    return {"data": resposta, "status": 200}
+            resposta.append(ClienteResponse(posicao=cliente.id, nome=cliente.nome, data_chegada=cliente.data))
+    return {"info": resposta, "status": 200}
 
 
 @app.get("/fila/{id}")
 def get_by_id(id: int):
     for cliente in db_clientes:
         if cliente.id == id:
-            return {"posicao": cliente.id, "nome": cliente.nome, "data_chegada": cliente.data}
+            return {
+                "info": ClienteResponse(posicao=cliente.id, nome=cliente.nome, data_chegada=cliente.data),
+                "status": 200
+            }
     return {"mensagem": "Cliente não localizado.", "status": 404}
+
+
+@app.post("/fila")
+def create(cliente: ClienteRequest):
+    novo_cliente = Cliente(
+        nome=cliente.nome,
+        tipo_atendimento=cliente.tipo_atendimento,
+        data=datetime.now(),
+        status_atendimento=False
+    )
+    novo_cliente.id = db_clientes[-1].id + 1
+    db_clientes.append(novo_cliente)
+    return {"mensagem": "Cliente adicionado na fila.", "status": 201}
